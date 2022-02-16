1f0bab4d0a0a743284a1ec2d99c22d33727db85c
halaxysounds <halaxysounds@gmail.com>
2022-02-16 04:47:47 -0300

    added animations, put onTouchEnd here

--- Components/NewMovie.js
+++ Components/NewMovie.js
@@ -1,36 +1,73 @@
 import React from "react";
-import { useContext } from "react";
-import { View, Image, Text, StyleSheet } from 'react-native'
+import { useContext, useState, useRef, useEffect } from "react";
+import { View, Image, Text, StyleSheet, Animated, Easing } from 'react-native'
+import { MovieContext } from "../App";
 
 import { ThemeContext } from "./Contexts/ThemeContext";
 
-export default function NewMovie({fullRes = false, bannerHeight = 180, bannerWidth = 120, imageLink, movieName}){
+export default function NewMovie({fullRes = false, movie, delay = 0}){
     const theme = useContext(ThemeContext)
+    const contextProps = useContext(MovieContext)
+
+    const slideAnim = useRef(new Animated.Value(200)).current
+    const opacityAnim = useRef(new Animated.Value(0)).current
+    
+    function playAnim(){
+        Animated.timing(slideAnim, {
+            toValue: 0,
+            duration: 750,
+            delay: delay,
+            useNativeDriver: true,
+            easing: Easing.out(Easing.exp)
+        }).start()
+        Animated.timing(opacityAnim, {
+            toValue: 1,
+            duration: 3000,
+            delay: delay,
+            useNativeDriver: true,
+            easing: Easing.out(Easing.exp)
+        }).start()
+    }
+    useEffect(() => {
+        playAnim()
+    }, [])
+
+
     const styles = StyleSheet.create({
         container: {
-            marginRight: 15
+            marginRight: 15,
+            transform: [{'translateY': slideAnim}],
+            opacity: opacityAnim
         },
         title: {
             marginTop: 5,
             textAlign: 'center',
-            width: bannerWidth,
+            width: 120,
             color: theme.foreground,
             fontFamily: theme.fontRegular,
         },
         banner: {
-            height: bannerHeight,
-            width: bannerWidth,
+            height: 180,
+            width: 120,
             borderRadius: 15,
-            resizeMode: 'cover'
+            backgroundColor: theme.accent
         }
     })
     
     return(
-        <View style={styles.container}>
-            <Image style={styles.banner} source={{uri: `https://image.tmdb.org/t/p/${fullRes ? 'original' : 'w500'}/${imageLink}`}}/>
+        <Animated.View
+            style={styles.container}
+            onTouchEnd={() => {
+                contextProps.setSelectedMovie(movie.id)
+                contextProps.setIsOnMovie(true)
+            }}
+        >
+            <Image
+                style={styles.banner}
+                source={{uri: `https://image.tmdb.org/t/p/${fullRes ? 'original' : 'w400'}/${movie.poster_path}`}}/>
             <Text style={styles.title}>
-                {movieName}
+                {movie.title}
             </Text>
-        </View>
+        </Animated.View>
     )
 }
