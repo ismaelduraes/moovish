047290dfee5b6ccd7d0313c4f0de078a42624e8c
halaxysounds <halaxysounds@gmail.com>
2022-02-16 04:46:16 -0300

    added navbar icons and refactor

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,34 +1,82 @@
 import React from 'react'
 import { useContext } from 'react'
 import { ThemeContext } from './Contexts/ThemeContext'
-import { Text, StyleSheet } from 'react-native'
+import { Text, StyleSheet, View } from 'react-native'
 import LinearGradient from 'react-native-linear-gradient'
-import { BlurView } from '@react-native-community/blur'
+import { default as Ionicons } from 'react-native-vector-icons/Ionicons'
+import { MovieContext } from '../App'
 
 export default function Nav({title = 'moovish'}){
     const theme = useContext(ThemeContext)
+    const contextProps = useContext(MovieContext)
+
+    const styles = StyleSheet.create({
+        container :{
+            height: '15%',
+            width: '100%',
+            justifyContent: 'space-evenly',
+            zIndex: 10
+        },
+        title: {
+            fontSize: 28,
+            fontFamily: theme.fontBold,
+            color: theme.foreground
+        },
+        navigation: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: '11%',
+            top: -30,
+        }
+    })
 
     return(
-        <LinearGradient colors={[theme.accent, 'rgba(0, 0, 0, 0)']} style={styles.container}>
-            <Text style={styles.title}>
-                {title}
-            </Text>
+        <LinearGradient
+            colors={[
+                theme.accent,
+                'rgba(0, 0, 0, 0)'
+                ]}
+            style={styles.container}
+        >
+            <View style={styles.navigation}>
+                <Ionicons
+                    name="ios-person-outline"
+                    size={30}
+                    color={theme.foreground}
+                />
+
+                <Text
+                    onTouchEnd={() => {
+                        contextProps.setIsOnSearch(false)
+                        contextProps.setIsOnMovie(false)
+                    }}
+                    style={styles.title}
+                >
+                    {title}
+                </Text>
+                
+                <View
+                    onTouchEnd={() => {
+                        contextProps.setIsOnSearch(!contextProps.isOnSearch)
+                        contextProps.setIsOnMovie(false)
+                    }}
+                >
+                    {
+                    contextProps.isOnSearch ?
+                    <Ionicons
+                        name="close-outline"
+                        size={30}
+                        color={theme.foreground}
+                    /> :
+                    <Ionicons
+                    name="ios-search-outline"
+                    size={30}
+                    color={theme.foreground}
+                    />
+                    }
+                </View>
+            </View>
         </LinearGradient>
     )
 }
-
-const styles = StyleSheet.create({
-    container :{
-        height: '15%',
-        width: '100%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        zIndex: 10
-    },
-    title: {
-        top: -40,
-        fontSize: 28,
-        fontFamily: 'AtkinsonHyperlegible-Bold',
-        color: 'white'
-    },
-})
