3af93fd4d9eb6e8f73355a98932a8370d56d9273
halaxysounds <halaxysounds@gmail.com>
2022-02-17 10:48:06 -0300

    bring nav to main, remove movie context, refactor

--- Components/Main.js
+++ Components/Main.js
@@ -1,35 +1,52 @@
 import React from "react";
-import { ScrollView, StyleSheet, View } from "react-native";
+import { useState, useContext } from "react";
+import {
+    ScrollView,
+    StyleSheet,
+    View,
+    Dimensions
+} from "react-native";
 import TopRated from "./TopRated";
 import Trending from "./Trending";
 import New from "./New";
 import NowPlaying from "./NowPlaying";
+import Nav from "./Nav";
+import { ThemeContext } from "./Contexts/ThemeContext";
 
-export default function Main(props){
+const width = Dimensions.get('window').width
+
+export default function Main({navigation}){
+    const [newLoaded, setNewLoaded] = useState(false)
+    const theme = useContext(ThemeContext)
+
     return(
-        <ScrollView
-        showsVerticalScrollIndicator={false}
-        style={styles.container}
-        contentContainerStyle={styles.contentContainer}
-        >
-            <New/>
-            <Trending/>
-            <NowPlaying/>
-            <TopRated/>
-        </ScrollView>
+        <View style={{...styles.container, backgroundColor: theme.background}}>
+            <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={{
+                backgroundColor: theme.background
+            }}
+            contentContainerStyle={styles.contentContainer}
+            >
+                {/* render new images before anything else */}
+                <New setNewLoaded={setNewLoaded}/>
+
+                {newLoaded &&
+                <View>
+                    <Trending/>
+                    <NowPlaying/>
+                    <TopRated/>
+                </View>
+                }
+            </ScrollView>
+            <Nav/>
+        </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        paddingTop: '25%',
-        // paddingHorizontal: '7%',
-        position: 'absolute',
-        top: 0,
-        bottom: 0,
-        // overflow: 'visible'
-        // left: 0,
-        // right: 0,
+        width: width,
     },
     contentContainer: {
         paddingBottom: '20%',
