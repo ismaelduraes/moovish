bde3a13a1aad696eb540912d62d8e517674947bd
halaxysounds <halaxysounds@gmail.com>
2022-02-17 11:01:22 -0300

    rename animations, remove boilerplate, removed see more button

--- Components/Trending.js
+++ Components/Trending.js
@@ -1,82 +1,95 @@
 import React from "react";
-import { useState, useContext, useEffect, useRef } from "react";
-import { Animated, View, Text, StyleSheet, Image, Dimensions, Easing } from "react-native";
+import {
+    useState,
+    useContext,
+    useEffect,
+    useRef
+} from "react";
+import {
+    Animated,
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    Dimensions,
+    LayoutAnimation
+} from "react-native";
+
+import { Link } from "react-router-native";
+
+import SlideAnimationFunction from "./Utilities/SlideAnimationFuncion";
 
 import { ThemeContext } from "./Contexts/ThemeContext";
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
 import Carousel from "react-native-snap-carousel";
-import LinearGradient from "react-native-linear-gradient";
-import { BlurView } from '@react-native-community/blur'
 import { MovieContext } from "../App";
 import { TMDB_API_KEY } from '@env'
+import { imgPrefixOriginal } from "./Utilities/Utilities";
 
 const width = Dimensions.get('window').width
-const height = Dimensions.get('window').height
 
 export default function Trending(){
     //states
     const theme = useContext(ThemeContext)
     const contextProps = useContext(MovieContext)
 
-    const opacityAnimRef = useRef(new Animated.Value(0)).current
-    const topAnimRef = useRef(new Animated.Value(25)).current
+    const slideAnim = useRef(new Animated.Value(25)).current
+    const containerSlideAnim = useRef(new Animated.Value(150)).current
 
     const [trendingMovies, setTrendingMovies] = useState([])
     const [activeSlide, setActiveSlide] = useState(0)
 
-    //function
-    function playAnim(animRef, to = 0, duration = 1000, useNative = true){
-        Animated.timing(animRef, {
-            toValue: to,
-            duration,
-            useNativeDriver: true,
-            easing: Easing.out(Easing.exp)
-        }).start()
-    }
+    //functions
     function fetchData(){
+        // console.log(`https://api.themoviedb.org/3/trending/movie/day?api_key=${TMDB_API_KEY}`)
         fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=${TMDB_API_KEY}`)
         .then(result => result.json()
         .then(data => setTrendingMovies(data.results))
         )
     }
-
-    useEffect(() => {
-        Animated.timing(opacityAnimRef).reset()
-        Animated.timing(topAnimRef).reset()
-        playAnim(opacityAnimRef, 1, 1000)
-        playAnim(topAnimRef, 0, 1000, false)
-    }, [activeSlide])
-
     
+    //run on mount
+    useEffect(() => {
+        SlideAnimationFunction(containerSlideAnim, 0, 1000)
+        fetchData()
+    }, [])
+    //
 
     useEffect(() => {
-        fetchData();
-    }, [])
+        // Animated.timing(opacityAnim).reset()
+        Animated.timing(slideAnim).reset()
+        SlideAnimationFunction(slideAnim, 0, 500)
+    }, [activeSlide])
+    
 
     const styles = StyleSheet.create({
         container: {
-            marginBottom: 25,
-            alignItems: 'center',
+            marginBottom: theme.homeComponentsBottomMargin,
             position: 'relative',
-            height: 750
+            transform: [{'translateY': containerSlideAnim}]
         },
         sectionTitle: {
             fontSize: 26,
             fontFamily: theme.fontBold,
-            color: theme.foreground,
-            paddingHorizontal: '7%',
-            marginBottom: 20,
+            color: theme.accentLight,
+            paddingHorizontal: theme.defaultPadding,
         },
         title: {
-            marginTop: '5%',
             fontSize: 20,
             color: theme.foreground,
+            fontFamily: theme.fontBold,
+            marginTop: 20,
+        },
+        caption: {
             fontFamily: theme.fontRegular,
+            marginBottom: '7%',
+            paddingHorizontal: theme.defaultPadding,
+            color: theme.foreground
         },
         banner: {
             backgroundColor: theme.accent,
             width: '100%',
-            height: 500,
+            height: 510,
             borderRadius: 15,
             resizeMode: 'cover',
         },
@@ -86,73 +99,26 @@ export default function Trending(){
             fontFamily: theme.fontRegular,
             width: '100%',
             alignSelf: 'center',
-            height: 100,
+            color: theme.foreground
         },
-        trendingGradient: {
-            width: '100%',
-            height: 200,
-            top: '35%',
-            position: 'absolute'
-        },
-        seeMore: {
-            position: 'absolute',
-            top: '85%',
-            width: '100%'
-        },
-        seeMoreText: {
-            position: 'absolute',
-            textAlign: 'center',
-            alignSelf: 'center',
-            borderRadius: 50,
-            height: 30,
-            lineHeight: 30,
-            width: '30%',
-            color: theme.foreground,
-            backgroundColor: theme.accent,
-            top: '20%',
-        },
-        blur: {
-            position: 'absolute',
-            height: '100%',
-            width: '100%',
-        }
     })
     
     function Banner(item){
         return(
-            <View>
-                <View
-                onTouchEnd={
-                    () => {
-                    contextProps.setIsOnMovie(true)
-                    contextProps.setSelectedMovie(item.item.id)
-                }}
-                style={{
-                    flexDirection: 'row',
-                    alignItems: 'center'
-                    }}
-                >
-                    <Image source={{uri: `https://image.tmdb.org/t/p/original${item.item.poster_path}`}} style={styles.banner}
+            <Link to={`/movie/${item.item.id}`}>
+            <View removeClippedSubviews>
+                    <Image
+                        source={{uri: `${imgPrefixOriginal}${item.item.poster_path}`}}
+                        style={styles.banner}
+                        progressiveRenderingEnabled
                     />
-
-                    {
-                    item.index === 0 &&
-                    <Icon
-                        name="arrow-right"
-                        size={24}
-                        style={{left: 5}}
-                    />
-                    }
-                </View>
-
                 {
                 activeSlide === item.index &&
                 <View>
                     <Animated.Text
                     style={{
                         ...styles.title,
-                        opacity: opacityAnimRef,
-                        transform: [{translateY: topAnimRef}]
+                        transform: [{translateY: slideAnim}],
                     }}>
                         {item.item.title}
                     </Animated.Text>
@@ -160,63 +126,48 @@ export default function Trending(){
                     <Animated.Text
                     style={{
                         ...styles.overview,
-                        opacity: opacityAnimRef,
-                        transform: [{translateY: topAnimRef}]
+                        transform: [{translateY: slideAnim}],
                     }}>
                         {item.item.overview}
                     </Animated.Text>
 
-                    <LinearGradient
-                    style={styles.trendingGradient}
-                    colors={
-                        ['rgba(0, 0, 0, 0)',
-                         'rgba(0, 0, 0, 1)',
-                         'rgba(0, 0, 0, 1)']
-                    }/>
-
-                    <Animated.View
-                        //open movie if such is the active slide and clicked
-                        //this conditon prevents users from opening the wrong slide
-                        //accidentaly
-                        onTouchEnd={(() => {
-                            if (item.index === activeSlide){
-                                contextProps.setIsOnMovie(true)
-                                contextProps.setSelectedMovie(item.item.id)
-                            }
-                        })}
-                        style={{
-                            ...styles.seeMore,
-                            opacity: opacityAnimRef,
-                            transform: [{translateY: topAnimRef}]
-                    }}>
-                        
-                        <Text style={{
-                            ...styles.seeMoreText,
-                            position: 'absolute'
-                        }}>
-                            See More
-                        </Text>
-
-                    </Animated.View>
                 </View>
                 }
             </View>
+            </Link>
         )
     }
 
     return(
-        <View style={styles.container}>
-            <Text style={styles.sectionTitle}>
+        <Animated.View style={styles.container}>
+            <Text style={{
+                ...styles.sectionTitle,
+                }}>
                 Trending
             </Text>
+            <Text style={{
+                ...styles.caption,
+                }}>
+                What everyone keeps talking about
+            </Text>
+
             <Carousel
                 data={trendingMovies}
                 renderItem={Banner}
                 sliderWidth={width}
-                itemWidth={width*0.85}
+                itemWidth={width-(theme.defaultPadding*2)}
                 layout="stack"
-                onSnapToItem={e => setActiveSlide(e)}
+                onSnapToItem={e => {
+                    setActiveSlide(e)
+                    LayoutAnimation.configureNext(
+                        {
+                            duration: 700,
+                            update: { type: 'spring', springDamping: 0.4 },
+                        }
+                    );
+                    }}
+                removeClippedSubviews
             />
-        </View>
+        </Animated.View>
     )
 }
