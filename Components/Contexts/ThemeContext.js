04c459d8497f6e049c0f8d807652172c50839e4e
halaxysounds <halaxysounds@gmail.com>
2022-02-17 11:01:43 -0300

    added more context

--- Components/Contexts/ThemeContext.js
+++ Components/Contexts/ThemeContext.js
@@ -1,14 +1,31 @@
 import { createContext } from "react";
 
 export const themes = {
-    default: {
-        accent: 'crimson',
-        accentLight: '8E9FDC',
+    light: {
+        type: 'light',
+        accent: 'blue',
+        accentLight: '#8E9FDC',
+        background: 'white',
+        foreground: 'black',
+        gray: 'lightgray',
+        fontRegular: 'AtkinsonHyperlegible',
+        fontBold: 'AtkinsonHyperlegible-Bold',
+        defaultPadding: 20,
+        borderRadius: 15,
+        homeComponentsBottomMargin: '15%'
+    },
+    dark: {
+        type: 'dark',
+        accent: '#2E282A',
+        accentLight: '#bfae78',
         background: 'black',
-        foreground: 'white',
+        foreground: '#F9E7E7',
         gray: '#0f0f0f',
         fontRegular: 'AtkinsonHyperlegible',
-        fontBold: 'AtkinsonHyperlegible-Bold'
+        fontBold: 'AtkinsonHyperlegible-Bold',
+        defaultPadding: 20,
+        borderRadius: 15,
+        homeComponentsBottomMargin: '15%'
     }
 }
 export const ThemeContext = createContext({})
