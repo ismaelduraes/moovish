67ca608a41a861f8564d7231ae99f6c81ea0b844
halaxysounds <halaxysounds@gmail.com>
2022-02-17 11:04:27 -0300

    added nowidth prefix for more options

--- Components/Utilities/Utilities.js
+++ Components/Utilities/Utilities.js
@@ -1,3 +1,4 @@
 export const imgPrefix = 'https://image.tmdb.org/t/p/w500'
 export const imgPrefixLow = 'https://image.tmdb.org/t/p/w500'
 export const imgPrefixOriginal = 'https://image.tmdb.org/t/p/original'
+export const imgPrefixNoWidth = 'https://image.tmdb.org/t/p/'
