65ca4324e0ad75d24bc010d43115980b0f173a23
halaxysounds <halaxysounds@gmail.com>
2022-02-17 10:53:58 -0300

    remove movie context, add more props, remove opacity anim

--- Components/NewMovie.js
+++ Components/NewMovie.js
@@ -1,33 +1,39 @@
 import React from "react";
-import { useContext, useState, useRef, useEffect } from "react";
-import { View, Image, Text, StyleSheet, Animated, Easing } from 'react-native'
-import { MovieContext } from "../App";
+import { useContext, useRef, useEffect } from "react";
+import { Image, Text, StyleSheet, Animated, Easing, Dimensions } from 'react-native'
+
+import { Link } from "react-router-native";
 
 import { ThemeContext } from "./Contexts/ThemeContext";
+import { imgPrefix, imgPrefixOriginal } from "./Utilities/Utilities";
 
-export default function NewMovie({fullRes = false, movie, delay = 0}){
+const width = Dimensions.get('window').width
+
+export default function NewMovie({
+    movie,
+    showText = true,
+    //↓disable if you want to add custom text
+    useBackdrop = false,
+    originalQuality = false,
+    width = 100,
+    //↓useful if instantiating many. you can set the delay to be a multiple of the
+    //index/key
+    animDelay = 0
+    }){
     const theme = useContext(ThemeContext)
-    const contextProps = useContext(MovieContext)
 
     const slideAnim = useRef(new Animated.Value(200)).current
-    const opacityAnim = useRef(new Animated.Value(0)).current
     
     function playAnim(){
         Animated.timing(slideAnim, {
             toValue: 0,
             duration: 750,
-            delay: delay,
-            useNativeDriver: true,
-            easing: Easing.out(Easing.exp)
-        }).start()
-        Animated.timing(opacityAnim, {
-            toValue: 1,
-            duration: 3000,
-            delay: delay,
+            delay: animDelay,
             useNativeDriver: true,
             easing: Easing.out(Easing.exp)
         }).start()
     }
+
     useEffect(() => {
         playAnim()
     }, [])
@@ -35,39 +41,41 @@ export default function NewMovie({fullRes = false, movie, delay = 0}){
 
     const styles = StyleSheet.create({
         container: {
-            marginRight: 15,
+            marginRight: 9,
             transform: [{'translateY': slideAnim}],
-            opacity: opacityAnim
+            width: width,
         },
         title: {
             marginTop: 5,
             textAlign: 'center',
-            width: 120,
             color: theme.foreground,
             fontFamily: theme.fontRegular,
         },
         banner: {
-            height: 180,
-            width: 120,
+            height: useBackdrop ? width*0.55 : width*1.6,
+            width: width,
             borderRadius: 15,
-            backgroundColor: theme.accent
+            backgroundColor: theme.accent,
         }
     })
     
     return(
+        <Link to={`/movie/${movie.id}`} state={{id: movie.id}}>
         <Animated.View
             style={styles.container}
-            onTouchEnd={() => {
-                contextProps.setSelectedMovie(movie.id)
-                contextProps.setIsOnMovie(true)
-            }}
         >
-            <Image
-                style={styles.banner}
-                source={{uri: `https://image.tmdb.org/t/p/${fullRes ? 'original' : 'w400'}/${movie.poster_path}`}}/>
-            <Text style={styles.title}>
+                <Image
+                    style={styles.banner}
+                    source={{uri: `${originalQuality ? imgPrefixOriginal : imgPrefix}${useBackdrop? movie.backdrop_path : movie.poster_path}`}}
+                    progressiveRenderingEnabled
+                    onError={e => console.log(e.nativeEvent)}
+                />
+            {showText && 
+            <Animated.Text style={styles.title}>
                 {movie.title}
-            </Text>
+            </Animated.Text>}
+
         </Animated.View>
+        </Link>
     )
 }
