6438c1b6d299be2a7803b6c23a49da946959a8d2
halaxysounds <halaxysounds@gmail.com>
2022-02-18 09:17:46 -0300

    set wait for load, add birth place, gender, images and blurred bg

--- Screens/ProfileScreen.js
+++ Screens/ProfileScreen.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { useState, useEffect, useContext } from "react";
+import { useState, useLayoutEffect, useContext } from "react";
 import {
     ScrollView,
     StyleSheet,
     View,
+    Image
 } from 'react-native'
 import { useParams } from "react-router-native";
 import axios from "axios";
@@ -13,44 +14,88 @@ import Header from "../Components/Header";
 import { ThemeContext } from "../Components/Contexts/ThemeContext";
 import TextBody from "../Components/TextBody";
 import Nav from "../Components/Nav";
+import ImageCarousel from "../Components/ImageCarousel";
+
+import { imgPrefixOriginal } from "../Components/Utilities/Utilities";
+import NavButtons from "../Components/NavButtons";
 
 export default function ProfileScreen(){
     const profileId = useParams().profileId
     const theme = useContext(ThemeContext)
 
     const [profileData, setProfileData] = useState({})
-
+    const [loaded, setLoaded] = useState(false)
+    
     
     function fetchData(){
         axios.get(`https://api.themoviedb.org/3/person/${profileId}?api_key=${TMDB_API_KEY}&append_to_response=images`)
-        .then(r => setProfileData(r.data))
+        .then(r => {
+            setProfileData(r.data)
+            setLoaded(true)
+        })
     }
     
-    useEffect(() => {
+    useLayoutEffect(() => {
         fetchData()
+        console.log(profileData)
     }, [])
     
     const styles = StyleSheet.create({
         container: {
-            backgroundColor: theme.background,
             height: '100%',
             width: '100%'
+        },
+        imageBg: {
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            opacity: 0.3
         }
     })
 
-    return(
-        <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
-            <Nav/>
-            <Header
-            title={profileData.name}
-            imagePath={profileData.profile_path}
-            subtitle={`Known for ${profileData.known_for_department}`}
+    return loaded ? (
+        <View style={{backgroundColor: theme.background}}>
+            <NavButtons/>
+            <Image
+            style={styles.imageBg}
+            source={{uri: `${imgPrefixOriginal}${profileData.profile_path}`}}
+            blurRadius={50}
             />
-            <TextBody
-            style={styles.section}
-            title={`About ${profileData.name}`}
-            text={profileData.biography ? profileData.biography : 'No biography is available for this person'}
-            />
-        </ScrollView>
-    )
+            <ScrollView showsVerticalScrollIndicator={false} style={styles.container}>
+                <Header
+                title={profileData.name}
+                imagePath={profileData.profile_path}
+                subtitle={`Known for ${profileData.known_for_department}`}
+                />
+                <TextBody
+                style={styles.section}
+                title="Biography"
+                text={profileData.biography ? profileData.biography : 'No biography is available for this person'}
+                hideIfLong
+                />
+                <View style={{flexDirection: 'row'}}>
+                    <TextBody
+                    style={styles.section}
+                    title="Born in"
+                    text={profileData.place_of_birth ? profileData.place_of_birth : 'Unknown'}
+                    hideIfLong
+                    marginBottom={50}
+                    width="50%"
+                    />
+                    <TextBody
+                    style={styles.section}
+                    title="Gender"
+                    text={profileData.gender === 1 ? 'Female' : 'Male'}
+                    hideIfLong
+                    width="50%"
+                    />
+                </View>
+                <ImageCarousel
+                data={profileData.images.profiles}
+                isSquare
+                canChangeResize
+                />
+            </ScrollView>
+        </View>
+    ) : null
 }
