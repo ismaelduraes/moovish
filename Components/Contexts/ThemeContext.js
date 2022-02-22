aab48e8dcda445fe06c4be9382838a7a60375d16
halaxysounds <halaxysounds@gmail.com>
2022-02-22 13:17:11 -0300

    update theme

--- Components/Contexts/ThemeContext.js
+++ Components/Contexts/ThemeContext.js
@@ -4,29 +4,29 @@ import { createContext } from "react";
 export const themes = {
     light: {
         type: 'light',
-        accent: '#9c0a00', //tomato-ish
+        accent: '#fc0303', //tomato-ish
         accentAlt: '#ffffff', //white
         background: '#ffffff', //black
         foreground: '#000000', //white
-        gray: '#0f0f0f',
+        gray: '#cccccc',
         fontRegular: 'AtkinsonHyperlegible',
         fontBold: 'AtkinsonHyperlegible-Bold',
         defaultPadding: 20,
         borderRadius: 12,
-        homeComponentsBottomMargin: '12%'
+        homeComponentsBottomMargin: '9%'
     },
     dark: {
         type: 'dark',
-        accent: '#d6be81',
+        accent: '#fc0303',
         accentAlt: '#000000', // black
         background: '#000000', //black
-        foreground: '#F9e7e7',
+        foreground: '#f9e7e7',
         gray: '#0f0f0f',
         fontRegular: 'AtkinsonHyperlegible',
         fontBold: 'AtkinsonHyperlegible-Bold',
         defaultPadding: 20,
         borderRadius: 15,
-        homeComponentsBottomMargin: '12%'
+        homeComponentsBottomMargin: '9%'
     }
 }
 export const ThemeContext = createContext({})
