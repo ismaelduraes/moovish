1fd4fa26f758ab5407fce852e8ba978c09cf96f6
halaxysounds <halaxysounds@gmail.com>
2022-02-23 12:10:49 -0300

    remove boilerplate, add authentication

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState, createContext } from 'react'
 import {
   StyleSheet,
@@ -17,15 +17,62 @@ import Main from './Screens/Main'
 import MovieScreen from './Screens/MovieScreen'
 import SearchScreen from './Screens/SearchScreen'
 import ProfileScreen from './Screens/ProfileScreen'
-import NavigationTab from './Components/NavigationTab'
+import Library from './Screens/Library'
+
+import RNSecureKeyStore from "react-native-secure-key-store";
 
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { PropsContext } from './Components/Contexts/PropsContext'
+import LoginScreen from './Screens/LoginScreen'
+import { AuthContext } from './Components/Contexts/AuthContext'
+import axios from 'axios'
 
 export const MovieContext = createContext()
 export default function App(){
   const [currentTheme, setCurrentTheme] = useState(themes.dark)
+  const [authTestDone, setAuthTestDone] = useState(false)
+  const [isAuth, setIsAuth] = useState(false)
+  const [token, setToken] = useState('')
+  const [loginData, setLoginData] = useState({})
+
+  async function checkAuth(){
+    await RNSecureKeyStore.get('auth_token')
+    .then(r => {
+      setToken(r)
+      //check token with server
+      axios.get('http://192.168.15.10:8080/profile/data', {headers: {'auth-token': r}})
+      //if success
+      .then(res => {
+          setIsAuth(true)
+          setLoginData(res.data)
+          console.log('authenticated')
+
+          axios.get('http://192.168.15.10:8080/profile/to_watch_list',
+          {headers: {'auth-token': r}})
+          .then(r => console.log(r.data))
+      })
+      //if error
+      .catch(error => {
+        if(error.code === 401){
+          ('Something went wrong and you have been logged out. Please log back in again.')
+        }
+        else{
+          alert('Something went wrong while trying to connect to moovish. Please check your internet connection.')
+        }
+          console.log('not authenticated')
+          setIsAuth(false)
+      })
+    })
+    .catch(err =>
+      {return}
+    )
+    setAuthTestDone(true)
+  }
+
+  useEffect(() => {
+    checkAuth()
+  }, [])
 
   if (Platform.OS === 'android') {
     if (UIManager.setLayoutAnimationEnabledExperimental) {
@@ -41,28 +88,36 @@ export default function App(){
     },
   })
 
-  return(
+  return authTestDone ? (
+    <AuthContext.Provider value={{
+      setToken,
+      token,
+      setIsAuth,
+      isAuth,
+      setLoginData,
+      loginData}}>
     <ThemeContext.Provider value={currentTheme}>
-    <SafeAreaProvider>
     <PropsContext.Provider value={{currentTheme, setCurrentTheme}}>
+    <SafeAreaProvider>
       <NavigationContainer>
   
         <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="navigationTab" component={NavigationTab}/>
           <Stack.Screen name="home" component={Main}/>
           <Stack.Screen name="movie" component={MovieScreen}/>
           <Stack.Screen name="profile" component={ProfileScreen}/>
           <Stack.Screen name="search" component={SearchScreen}/>
-          <Stack.Screen name="login" component={SearchScreen}/>
+          <Stack.Screen name="login" component={LoginScreen}/>
+          <Stack.Screen name="library" component={Library}/>
         </Stack.Navigator>
 
       </NavigationContainer>
-    </PropsContext.Provider>
     </SafeAreaProvider>
+    </PropsContext.Provider>
     <StatusBar
     backgroundColor='transparent' translucent barStyle={
     currentTheme.type == 'light' ? 'dark-content' : 'light-content'}
     />
     </ThemeContext.Provider>
-  )
+    </AuthContext.Provider>
+  ) : null
 }
