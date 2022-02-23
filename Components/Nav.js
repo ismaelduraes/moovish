f1b6a6d8f6a8336095bbd1738fbebd19416045ee
halaxysounds <halaxysounds@gmail.com>
2022-02-23 12:15:01 -0300

    restyle nav

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,86 +1,112 @@
 import React from 'react'
 import { useContext } from 'react'
 import { ThemeContext } from './Contexts/ThemeContext'
-import { Text, StyleSheet, View } from 'react-native'
+import {
+    Text,
+    StyleSheet,
+    View
+} from 'react-native'
 
+import { useNavigation } from '@react-navigation/native'
+import { default as MaterialIcons } from 'react-native-vector-icons/MaterialIcons'
 import { default as MaterialCommunityIcons } from 'react-native-vector-icons/MaterialCommunityIcons'
-import { default as Feather } from 'react-native-vector-icons/Feather'
+
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { PropsContext } from './Contexts/PropsContext'
+import { AuthContext } from './Contexts/AuthContext'
 import { themes } from './Contexts/ThemeContext'
 
 export default function Nav({isOnSearch = false, title = 'moovish'}){
     const theme = useContext(ThemeContext)
+    const contextAuth = useContext(AuthContext)
     const contextProps = useContext(PropsContext)
+    const navigation = useNavigation()
 
     const styles = StyleSheet.create({
         container :{
-            zIndex: 10,
-            position: 'absolute',
-            // height: '100%',
+            zIndex: 1,
             width: '100%',
-            overflow: 'hidden',
-            backgroundColor: theme.background
+
+            marginTop: 20,
+            marginBottom: 10,
+            position: 'absolute',
+
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingHorizontal: theme.defaultPadding,
         },
         title: {
-            fontSize: 32,
+            fontSize: 30,
             fontFamily: theme.fontBold,
             color: theme.foreground,
+            textAlign: 'left'
         },
         navigation: {
             flexDirection: 'row',
             justifyContent: 'space-between',
-            height:70,
             overflow: 'hidden',
             zIndex: 1,
             paddingHorizontal: theme.defaultPadding,
             alignItems: 'center',
-            // backgroundColor: theme.background,
         },
-        blur: {
-            width: '100%',
-            height: '100%',
-            position: 'absolute'
+        searchBar: {
+            width: '70%',
+            backgroundColor: theme.gray,
+            alignItems: 'center',
+            justifyContent: 'center',
+            padding: 10,
+            borderRadius: theme.borderRadius,
+            flexDirection: 'row'
+        },
+        searchBarText: {
+            color: theme.foreground,
+            fontFamily: theme.fontRegular
         }
     })
 
     return(
-        <SafeAreaView style={styles.container} pointerEvents='box-none'>
-            {/* <BlurView
-            style={styles.blur}
-            blurAmount={32}
-            overlayColor={`${theme.type === 'light' ? '#ffffffb3' : '#000000cc'}`}
-            /> */}
-            <View style={styles.navigation}>
-                <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                    <MaterialCommunityIcons
-                    name="movie-open-outline"
-                    size={30}
-                    color={theme.accent}
-                    style={{marginRight: 15}}
-                    />
-                    <Text style={styles.title}>
-                        {title}
-                    </Text>
-                </View>
-                <View
-                onTouchEnd={
-                    () => {
-                        contextProps.setCurrentTheme(
-                            contextProps.currentTheme.type === 'light' ?
-                            themes.dark :
-                            themes.light
-                        )
-                    }
-                }
-                >
-                    <Feather
-                    name="sun"
-                    color={theme.foreground}
-                    size={30}
-                    />
-                </View>
+        <SafeAreaView style={styles.container}>
+            <View
+                onTouchEnd={() => {
+                    console.log('sad')
+                    contextProps.setCurrentTheme(contextProps.currentTheme.type === 'light' ?
+                    themes.dark : themes.light
+                    )
+                }}
+                style={{flexDirection: 'row', alignItems: 'center'}}
+            >
+                <MaterialCommunityIcons
+                name='movie-open'
+                size={25}
+                color={theme.foreground}
+                style={{marginRight: 15}}
+                />
+                {/* <Text style={styles.title}>
+                    {title}
+                </Text> */}
             </View>
+            <View
+            onTouchEnd={() => navigation.navigate('search')}
+            style={styles.searchBar}
+            >
+                <MaterialIcons
+                name='search'
+                size={15}
+                color={theme.foreground}
+                style={{marginRight: 15}}
+                />
+                <Text style={styles.searchBarText}>
+                    My favorite movie is...
+                </Text>
+            </View>
+
+            <MaterialCommunityIcons
+            name='bookshelf'
+            size={25}
+            color={theme.foreground}
+            onTouchEnd={() => contextAuth.isAuth ? navigation.push('library') : navigation.push('login')}
+            />
         </SafeAreaView>
     )
 }
