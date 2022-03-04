acf3ccd8c5a61c151c20003863558f9e7202bd80
halaxysounds <halaxysounds@gmail.com>
2022-03-03 22:51:05 -0300

    added callback parameter

--- Components/Utilities/SlideAnimationFuncion.js
+++ Components/Utilities/SlideAnimationFuncion.js
@@ -1,11 +1,11 @@
 import { Animated, Easing } from 'react-native'
 
-export default function SlideAnimationFunction(animRef, to = 0, duration = 1000, useNativeDriver = true, delay = 0){
+export default function SlideAnimationFunction(animRef, to = 0, duration = 1000, useNativeDriver = true, delay = 0, callback){
     Animated.timing(animRef, {
         toValue: to,
         duration,
         useNativeDriver: useNativeDriver,
         delay: delay,
         easing: Easing.out(Easing.exp)
-    }).start()
+    }).start(() => callback)
 }
