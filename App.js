ee4eefa61b6e9de48fcda26d4da3e5197ed43531
halaxysounds <halaxysounds@gmail.com>
2022-03-04 00:14:00 -0300

    small refactoring

--- App.js
+++ App.js
@@ -29,48 +29,47 @@ import { AuthContext } from './Components/Contexts/AuthContext'
 import axios from 'axios'
 
 export const MovieContext = createContext()
-export default function App(){
+export default function App() {
   const [currentTheme, setCurrentTheme] = useState(themes.dark)
   const [authTestDone, setAuthTestDone] = useState(false)
   const [isAuth, setIsAuth] = useState(false)
   const [token, setToken] = useState('')
   const [loginData, setLoginData] = useState({})
 
-  function checkAuth(){
+  function checkAuth() {
     RNSecureKeyStore.get('auth_token')
-    .then(r => {
-      setToken(r)
-      console.log('token:',r)
-      //check token with server
-      axios.get('http://192.168.15.10:8080/profile/data', {headers: {'auth-token': r}})
-      //if success
-      .then(res => {
-          console.log('token apparently retrieved')
-          setIsAuth(true)
-          setLoginData(res.data)
-          setAuthTestDone(true)
+      .then(r => {
+        setToken(r)
+        console.log('token:', r)
+        //check token with server
+        axios.get('http://192.168.15.10:8080/profile/data', { headers: { 'auth-token': r } })
+          //if success
+          .then(res => {
+            console.log('token apparently retrieved')
+            setIsAuth(true)
+            setLoginData(res.data)
+            setAuthTestDone(true)
+          })
+          //if error
+          .catch(error => {
+            if (error.code === 401) {
+              //conection has been denied by server; log user out
+              alert('Something went wrong and you have been logged out. Please log back in again.')
+              setIsAuth(false)
+            }
+            else {
+              //has token but couldnt connect to moovish servers
+              alert('Something went wrong while trying to connect to moovish. Please check your internet connection.')
+              setIsAuth(true)
+            }
+            alert('not authenticated')
+            setAuthTestDone(true)
+          })
       })
-      //if error
-      .catch(error => {
-        if(error.code === 401){
-          //conection has been denied by server; log user out
-          alert('Something went wrong and you have been logged out. Please log back in again.')
-          setIsAuth(false)
-        }
-        else{
-          //has token but couldnt connect to moovish servers
-          alert('Something went wrong while trying to connect to moovish. Please check your internet connection.')
-          setIsAuth(true)
-        }
-          alert('not authenticated')
-          setAuthTestDone(true)
-      })
-    })
-    .catch(err =>
-      {
+      .catch(err => {
         setAuthTestDone(true)
       }
-    )
+      )
   }
 
   useEffect(() => {
@@ -98,29 +97,30 @@ export default function App(){
       setIsAuth,
       isAuth,
       setLoginData,
-      loginData}}>
-    <ThemeContext.Provider value={currentTheme}>
-    <PropsContext.Provider value={{currentTheme, setCurrentTheme}}>
-    <SafeAreaProvider>
-      <NavigationContainer>
-  
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="home" component={Main}/>
-          <Stack.Screen name="movie" component={MovieScreen}/>
-          <Stack.Screen name="profile" component={ProfileScreen}/>
-          <Stack.Screen name="search" component={SearchScreen}/>
-          <Stack.Screen name="login" component={LoginScreen}/>
-          <Stack.Screen name="library" component={Library}/>
-        </Stack.Navigator>
+      loginData
+    }}>
+      <ThemeContext.Provider value={currentTheme}>
+        <PropsContext.Provider value={{ currentTheme, setCurrentTheme }}>
+          <SafeAreaProvider>
+            <NavigationContainer>
 
-      </NavigationContainer>
-    </SafeAreaProvider>
-    </PropsContext.Provider>
-    <StatusBar
-    backgroundColor='transparent' translucent barStyle={
-    currentTheme.type == 'light' ? 'dark-content' : 'light-content'}
-    />
-    </ThemeContext.Provider>
+              <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="home" component={Main} />
+                <Stack.Screen name="movie" component={MovieScreen} />
+                <Stack.Screen name="profile" component={ProfileScreen} />
+                <Stack.Screen name="search" component={SearchScreen} />
+                <Stack.Screen name="login" component={LoginScreen} />
+                <Stack.Screen name="library" component={Library} />
+              </Stack.Navigator>
+
+            </NavigationContainer>
+          </SafeAreaProvider>
+        </PropsContext.Provider>
+        <StatusBar
+          backgroundColor='transparent' translucent barStyle={
+            currentTheme.type == 'light' ? 'dark-content' : 'light-content'}
+        />
+      </ThemeContext.Provider>
     </AuthContext.Provider>
   ) : null
 }
