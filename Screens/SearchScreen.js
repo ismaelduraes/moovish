666e968899cf4710c956adc2ef8e36faab418659
halaxysounds <halaxysounds@gmail.com>
2022-03-23 12:14:13 -0300

    added masonry layout

--- Screens/SearchScreen.js
+++ Screens/SearchScreen.js
@@ -13,36 +13,43 @@ import {
     ScrollView,
     StyleSheet,
     Image,
-    Dimensions
+    Dimensions,
+    Pressable
 } from 'react-native'
-import { useNavigation } from "@react-navigation/native"
 
+import MasonryList from '@react-native-seoul/masonry-list';
+import { useNavigation } from "@react-navigation/native"
 import { default as MaterialIcons } from 'react-native-vector-icons/MaterialIcons'
 import { default as Feather } from 'react-native-vector-icons/Feather'
 
-import { ThemeContext } from "../Components/Contexts/ThemeContext"
-
 import axios from "axios"
 
+import { ThemeContext } from "../Components/Contexts/ThemeContext"
 import { TMDB_API_KEY } from '@env'
 import { imgPrefix } from "../Components/Utilities/Utilities"
 import { SafeAreaView } from "react-native-safe-area-context"
 const width = Dimensions.get('window').width
 
-export default function SearchScreen(){
+export default function SearchScreen() {
     const theme = useContext(ThemeContext)
     const navigation = useNavigation()
     const searchInput = useRef()
+
+    const [isLoading, setIsLoading] = useState(false)
 
     const [results, setResults] = useState([])
     const [query, setQuery] = useState('')
     const [input, setInput] = useState('')
 
-    function fetchData(){
+    function fetchData() {
+        setIsLoading(true)
         axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_API_KEY}&query=${query}&page=1`)
-        .then(r => setResults(r.data.results))
+            .then(r => {
+                setIsLoading(false)
+                setResults(r.data.results)
+            })
     }
-    
+
     useEffect(() => {
         if (query) fetchData()
     }, [query])
@@ -76,7 +83,7 @@ export default function SearchScreen(){
             color: theme.foreground,
             fontSize: 30,
             marginBottom: 15,
-            marginTop: 50
+            marginTop: 50,
         },
         searchBar: {
             paddingHorizontal: 15,
@@ -115,23 +122,48 @@ export default function SearchScreen(){
         },
     })
 
-    return(
+    function ResultItem({ item, index }) {
+        return (
+            <Pressable
+                style={{
+                    width: '90%',
+                    alignSelf: 'center'
+                }}
+                onPress={() => navigation.push('movie', { movieId: item.id })}>
+                <View>
+                    <Image
+                        source={{ uri: `${imgPrefix}${item.popularity > 150 ? item.backdrop_path : item.poster_path}` }}
+                        style={{
+                            ...styles.resultImage,
+                            width: '100%',
+                            height: 220
+                        }}
+                    />
+                    <Text style={styles.resultText}>
+                        {item.title}
+                    </Text>
+                </View>
+            </Pressable>
+        )
+    }
+
+    return (
         <SafeAreaView style={styles.container}>
 
             <View style={styles.searchBarContainer}>
                 <Feather
-                name="arrow-left"
-                size={25}
-                color={theme.foreground}
-                style={{width: '10%'}}
-                onTouchEnd={() => navigation.navigate('home')}
+                    name="arrow-left"
+                    size={25}
+                    color={theme.foreground}
+                    style={{ width: '10%' }}
+                    onTouchEnd={() => navigation.navigate('home')}
                 />
                 <View style={styles.searchBar}>
                     <MaterialIcons
-                    name='search'
-                    size={15}
-                    color={theme.foreground}
-                    style={{marginRight: 15}}
+                        name='search'
+                        size={15}
+                        color={theme.foreground}
+                        style={{ marginRight: 15 }}
                     />
                     <TextInput
                         ref={searchInput}
@@ -144,45 +176,19 @@ export default function SearchScreen(){
                         onEndEditing={() => {
                             setQuery(input)
                         }}
-                        onLayout={() => {searchInput.current.focus()}}
+                        onLayout={() => { searchInput.current.focus() }}
                     />
                 </View>
             </View>
-
-            <ScrollView
-                style={styles.scrollView}
-                contentContainerStyle={styles.scrollViewContainer}
-                showsVerticalScrollIndicator={false}
-            >
-                {
-                    results.map((item, index) => {
-                        if (item.backdrop_path || item.poster_path)
-                        return(
-                            <View
-                            key={index}
-                            style={{
-                                ...styles.result,
-                                width: item.popularity > 150 ? '100%' : '50%',
-                                height: item.popularity > 200 ? 300 : 400
-                            }}
-                            >
-                                <View onTouchEnd={() => navigation.push('movie', {movieId: item.id})}>
-                                        <Image
-                                        source={{uri: `${imgPrefix}${item.popularity > 150 ? item.backdrop_path : item.poster_path}`}}
-                                        style={{
-                                            ...styles.resultImage,
-                                            width: item.popularity > 150 ? width-30 : (width/2)-30,
-                                        }}
-                                        />
-                                    <Text style={styles.resultText}>
-                                        {item.title}
-                                    </Text>
-                                </View>
-                            </View>
-                        )
-                    })
-                }
-            </ScrollView>
+            {/* {console.log(results)} */}
+            <MasonryList
+                data={results}
+                renderItem={ResultItem}
+                numColumns={2}
+                containerStyle={{ marginTop: 30, paddingHorizontal: theme.defaultPadding }}
+                onRefresh={() => fetchData}
+                refreshing={isLoading}
+            />
         </SafeAreaView>
     )
 }
