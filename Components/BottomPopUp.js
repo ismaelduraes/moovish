9d645af3d6f507b9d5b0ae7ac9a021819204e094
halaxysounds <halaxysounds@gmail.com>
2022-03-23 12:07:57 -0300

    fix cleanup

--- Components/BottomPopUp.js
+++ Components/BottomPopUp.js
@@ -1,66 +1,73 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import {
     StyleSheet,
     Dimensions,
     Text,
     Animated,
-    Easing
+    Easing,
+    View,
+    SafeAreaView
 } from 'react-native'
-import { SafeAreaView } from "react-native-safe-area-context";
+// import {  } from "react-native-safe-area-context";
 import { ThemeContext } from "./Contexts/ThemeContext";
 
-import { BlurView } from "@react-native-community/blur";
 import SlideAnimationFunction from "./Utilities/SlideAnimationFuncion";
 
 const height = Dimensions.get('window').height
 
-export default function BottomPopUp({setPopUpState, popUpState}){
+export default function BottomPopUp({ setPopUpState, popUpState }) {
     const theme = useContext(ThemeContext)
     const slideAnim = useRef(new Animated.Value(100)).current
 
     //waits for 3.5 seconds and then sets popup as inactive
-    async function unmount(){
+    async function unmount() {
         const timeout = await setTimeout(() => {
-            console.log('done')
             Animated.timing(slideAnim, {
                 toValue: 100,
                 duration: 1000,
                 useNativeDriver: true,
                 easing: Easing.out(Easing.exp)
-            }).start(() => setPopUpState({isActive: false}))
+            }).start(() => clear())
 
         }, 3500)
 
-        return () => {
+        clear = () => {
             Animated.timing(slideAnim).stop()
             clearTimeout(timeout)
+            setPopUpState({ isActive: false })
         }
         //slide down and set as inactive (from callback)
     }
 
-    SlideAnimationFunction(slideAnim, 0, 500, true)
+    SlideAnimationFunction(slideAnim, 0, 1000, true)
 
     useEffect(() => {
         unmount()
     }, [])
-    
+
     const styles = StyleSheet.create({
         container: {
-            height: '100%',
-            width: '100%',
             position: 'absolute',
+            height: height,
+            width: '100%',
             zIndex: 10,
+            alignSelf: 'center',
+            flex: 1,
+            justifyContent: 'flex-end'
         },
         popUp: {
             zIndex: 10,
             height: 60,
             width: '100%',
-            top: height-60,
+            // marginTop: height - 60,
+
+            // bottom: height - 60,
+            paddingBottom: 15,
 
             backgroundColor: theme.background,
 
-            transform: [{translateY: slideAnim}],
-            
+            transform: [{ translateY: slideAnim }],
+
             alignItems: 'center',
             justifyContent: 'center',
             alignSelf: 'center',
@@ -79,15 +86,15 @@ export default function BottomPopUp({setPopUpState, popUpState}){
         }
     })
 
-    return(
-        <Animated.View>
-            <SafeAreaView pointerEvents="box-none" style={styles.container}>
-                <Animated.View style={styles.popUp}>
-                    <Text style={styles.text}>
-                        {popUpState.text}
-                    </Text>
-                </Animated.View>
-            </SafeAreaView>
-        </Animated.View>
+    return (
+        // <Animated.View>
+        <SafeAreaView pointerEvents="box-none" style={styles.container}>
+            <Animated.View style={styles.popUp}>
+                <Text style={styles.text}>
+                    {popUpState.text}
+                </Text>
+            </Animated.View>
+        </SafeAreaView>
+        // </Animated.View>
     )
 }
