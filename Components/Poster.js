8c648880135a7a71e677b126fe7ea35b61decd36
halaxysounds <halaxysounds@gmail.com>
2022-03-23 12:16:58 -0300

    move to pressable, add gradient, borderRadius optional

--- Components/Poster.js
+++ Components/Poster.js
@@ -10,6 +10,7 @@ import {
     Easing,
     Dimensions,
     Image,
+    View,
     TouchableOpacity
 } from 'react-native'
 
@@ -18,20 +19,26 @@ import { useNavigation } from "@react-navigation/native";
 
 import { ThemeContext } from "./Contexts/ThemeContext";
 import { imgPrefix, imgPrefixOriginal } from "./Utilities/Utilities";
+import Pressable from "react-native/Libraries/Components/Pressable/Pressable";
+import LinearGradient from "react-native-linear-gradient";
 
 const width = Dimensions.get('window').width
 
 export default function Poster({
     movie,
-    showText = true,
     //↓disable if you want to add custom text
+    showText = true,
+    showGradient = false,
     useBackdrop = false,
     originalQuality = false,
     width = 100,
+    height,
     //↓useful if instantiating many. you can set the delay to be a multiple of the
     //index/key
     animDelay = 0,
-    animate = true
+    animate = true,
+    //border radius
+    isRounded = true
 }) {
     const theme = useContext(ThemeContext)
     const navigation = useNavigation()
@@ -59,16 +66,25 @@ export default function Poster({
             transform: [{ 'translateY': slideAnim }],
             width: width,
         },
+        gradient: {
+            width: '100%',
+            top: '80%',
+            height: '20%',
+            position: 'absolute',
+            zIndex: 1,
+        },
         title: {
             marginTop: 5,
             textAlign: 'center',
             color: theme.foreground,
             fontFamily: theme.fontRegular,
+            maxHeight: 45
         },
         banner: {
-            height: useBackdrop ? width * 0.55 : width * 1.5,
+            //if height was defined, use it, if not, set height depending on whether image is backdrop or poster
+            height: height ? height : (useBackdrop ? width * 0.55 : width * 1.5),
             width: width,
-            borderRadius: theme.borderRadius,
+            borderRadius: isRounded ? theme.borderRadius : 0,
             backgroundColor: theme.accent,
         }
     })
@@ -79,20 +95,26 @@ export default function Poster({
             key={movie.id}
             removeClippedSubviews
         >
-            <TouchableOpacity
+            <Pressable
                 onPress={() =>
                     navigation.push('movie', { movieId: movie.id })
                 }
             >
-                <FastImage
-                    style={styles.banner}
-                    source={{
-                        uri: `${originalQuality ? imgPrefixOriginal : imgPrefix}${useBackdrop ? movie.backdrop_path : movie.poster_path}`,
-                        priority: FastImage.priority.high
-                    }
-                    }
-                />
-            </TouchableOpacity>
+                <View>
+                    {showGradient ? <LinearGradient
+                        colors={['rgba(0,0,0,0)', theme.background]}
+                        style={styles.gradient}
+                    /> : null}
+                    <FastImage
+                        style={styles.banner}
+                        source={{
+                            uri: `${originalQuality ? imgPrefixOriginal : imgPrefix}${useBackdrop ? movie.backdrop_path : movie.poster_path}`,
+                            priority: FastImage.priority.high
+                        }
+                        }
+                    />
+                </View>
+            </Pressable>
             {showText &&
                 <Animated.Text style={styles.title}>
                     {movie.title}
