564ac21b764162751afe15690d4a094e01c1fb2a
halaxysounds <halaxysounds@gmail.com>
2022-03-29 17:40:49 -0300

    add inline env vars

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ["module:react-native-dotenv", {
+  plugins: [[
+    "module:react-native-dotenv", {
       "moduleName": "@env",
       "path": ".env",
       "blacklist": null,
@@ -9,10 +9,15 @@ module.exports = {
       "safe": false,
       "allowUndefined": true
     }],
+  ["transform-inline-environment-variables", {
+    "include": [
+      "NODE_ENV"
+    ]
+  }]
   ],
   env: {
     production: {
-      plugins: ["transform-remove-console"]
+      plugins: ["transform-inline-environment-variables", "transform-remove-console"]
     }
   }
 };
