7aa29e54f3b30c4f06caa9d07a16f5196f9f11d2
halaxysounds <halaxysounds@gmail.com>
2022-04-02 17:55:29 -0300

    add support for custom icons and text

--- Components/Loading.js
+++ Components/Loading.js
@@ -1,75 +1,81 @@
-import React, { useContext, useRef } from "react";
+import React, {useContext, useRef} from 'react';
 import {
-    View,
-    StyleSheet,
-    Animated,
-    Easing,
-    Text,
-} from 'react-native'
+  View,
+  StyleSheet,
+  Animated,
+  Easing,
+  Text,
+  Dimensions,
+} from 'react-native';
+const height = Dimensions.get('window').height;
+const width = Dimensions.get('window').width;
 
-import { default as MaterialCommunityIcons } from 'react-native-vector-icons/MaterialCommunityIcons'
-import { ThemeContext } from "./Contexts/ThemeContext";
+import {default as MaterialCommunityIcons} from 'react-native-vector-icons/MaterialCommunityIcons';
+import {ThemeContext} from './Contexts/ThemeContext';
 
-export default function Loading({ isError = false }) {
-    const theme = useContext(ThemeContext)
+export default function Loading({
+  isError = false,
+  rotate = true,
+  loadingText = 'Loading...',
+  iconName = 'loading',
+  errorText = 'Something went wrong.\n\nPlease check your internet connection',
+}) {
+  const theme = useContext(ThemeContext);
 
-    const rotateAnim = useRef(new Animated.Value(0)).current
+  const rotateAnim = useRef(new Animated.Value(0)).current;
+  const scaleAnim = useRef(new Animated.Value(0)).current;
 
-    const animTiming = Animated.timing(rotateAnim, {
-        toValue: 1,
-        duration: 1000,
-        useNativeDriver: true,
-        easing: Easing.bounce
-    })
+  const animTiming = ref =>
+    Animated.timing(ref, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+      easing: Easing.bounce,
+    });
+  if (rotate) Animated.loop(animTiming(rotateAnim)).start();
+  if (!rotate) animTiming(scaleAnim).start();
 
-    Animated.loop(animTiming).start()
+  const styles = StyleSheet.create({
+    container: {
+      position: 'absolute',
+      height,
+      width,
+      backgroundColor: theme.background,
+      zIndex: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    text: {
+      fontFamily: theme.fontBold,
+      fontSize: 20,
+      color: theme.foreground,
+      marginTop: 15,
+      textAlign: 'center',
+      paddingHorizontal: theme.defaultPadding,
+    },
+  });
 
-    const styles = StyleSheet.create({
-        container: {
-            position: 'absolute',
-            height: '100%',
-            width: '100%',
-            backgroundColor: theme.background,
-            zIndex: 10,
-            alignItems: 'center',
-            justifyContent: 'center'
-        },
-        text: {
-            fontFamily: theme.fontBold,
-            fontSize: 20,
-            color: theme.foreground,
-            marginTop: 15,
-            textAlign: 'center',
-            paddingHorizontal: theme.defaultPadding
-        }
-    })
-
-    return (
-        <View style={styles.container}>
-            <Animated.View
-                style={{
-                    transform: [
-                        {
-                            rotateZ: rotateAnim.interpolate({
-                                inputRange: [0, 1],
-                                outputRange: ['0deg', '360deg']
-                            })
-                        }
-                    ]
-                }}
-            >
-                <MaterialCommunityIcons
-                    name={isError ? "close" : "loading"}
-                    size={50}
-                    color={theme.accent}
-                />
-            </Animated.View>
-            <Text style={styles.text}>
-                {
-                    isError ? "Something went wrong.\n\nPlease check your internet connection"
-                        : "Loading..."
-                }
-            </Text>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          transform: [
+            {
+              rotateZ: rotateAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: ['0deg', '360deg'],
+              }),
+              scale: rotate ? 1 : scaleAnim,
+            },
+          ],
+        }}>
+        <MaterialCommunityIcons
+          name={isError ? 'close' : iconName}
+          size={50}
+          color={theme.accent}
+        />
+      </Animated.View>
+      <Text style={styles.text}>{isError ? errorText : loadingText}</Text>
+    </View>
+  );
 }
