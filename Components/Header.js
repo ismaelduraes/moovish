726e44f06e1d73cf64c2712361147ef617fd977f
halaxysounds <halaxysounds@gmail.com>
2022-04-02 21:03:14 -0300

    remove title and subtitle from header for parallax effect

--- Components/Header.js
+++ Components/Header.js
@@ -1,121 +1,96 @@
-import React from "react";
-import { useContext, useState } from "react";
+import React from 'react';
+import {useContext, useState} from 'react';
 import {
-    View,
-    Text,
-    StyleSheet,
-    Dimensions,
-    ActivityIndicator
-} from 'react-native'
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  ActivityIndicator,
+} from 'react-native';
 
-import { imgPrefixOriginal } from "./Utilities/Utilities";
+import {imgPrefixOriginal} from './Utilities/Utilities';
 
-import FastImage from "react-native-fast-image";
+import FastImage from 'react-native-fast-image';
 
-import { ThemeContext } from "./Contexts/ThemeContext";
+import {ThemeContext} from './Contexts/ThemeContext';
 
-import LinearGradient from "react-native-linear-gradient";
+import LinearGradient from 'react-native-linear-gradient';
 
-const width = Dimensions.get('window').width
+const width = Dimensions.get('window').width;
 
-export default function Header({ imagePath, fallbackImagePath, title, subtitle, resizeMode = "cover", tintColor = null }) {
-    const theme = useContext(ThemeContext)
-    const [isImageLoading, setIsImageLoading] = useState(true)
+export default function Header({
+  imagePath,
+  fallbackImagePath,
+  title,
+  subtitle,
+  resizeMode = 'cover',
+  tintColor = null,
+}) {
+  const theme = useContext(ThemeContext);
+  const [isImageLoading, setIsImageLoading] = useState(true);
 
-    const styles = StyleSheet.create({
-        posterContainer: {
-            width: '100%',
-            height: 350,
-            paddingHorizontal: resizeMode === "contain" ? theme.defaultPadding : 0,
-            backgroundColor: theme.accent,
-            marginBottom: 25,
-            alignContent: 'center',
-            justifyContent: 'center',
-        },
-        poster: {
-            width: '100%',
-            height: '100%',
-        },
-        titleContainer: {
-            // alignItems: 'center',
-            paddingHorizontal: theme.defaultPadding,
-            // top: -25,
-            zIndex: 2,
-            // marginBottom: 20,
-        },
-        headerTitle: {
-            color: theme.accent,
-            fontFamily: theme.fontBold,
-            fontSize: 24,
-            marginBottom: 5
-        },
-        subtitle: {
-            width: '80%',
-            color: theme.foreground,
-            fontFamily: theme.fontRegular
-        },
-        headerGradient: {
-            height: 350,
-            width: '100%',
-            position: 'absolute',
-            left: 0,
-            zIndex: 1,
-            opacity: 0.5,
-        },
-    })
+  const styles = StyleSheet.create({
+    posterContainer: {
+      width: '100%',
+      height: 350,
+      paddingHorizontal: resizeMode === 'contain' ? theme.defaultPadding : 0,
+      backgroundColor: theme.accent,
+      marginBottom: 25,
+    },
+    poster: {
+      width: '100%',
+      height: '100%',
+    },
 
-    return (
-        <View>
-            <View
-                style={styles.posterContainer}
-            >
-                {isImageLoading ?
-                    <ActivityIndicator
-                        style={{
-                            position: 'absolute',
-                            alignSelf: 'center',
-                            zIndex: 1,
-                        }}
-                        size="large"
-                        color={theme.background}
-                    /> : null
+    headerGradient: {
+      height: 350,
+      width: '100%',
+      position: 'absolute',
+      left: 0,
+      zIndex: 1,
+      opacity: 0.8,
+    },
+  });
+
+  return (
+    <View>
+      <View style={styles.posterContainer}>
+        {isImageLoading ? (
+          <ActivityIndicator
+            style={{
+              position: 'absolute',
+              alignSelf: 'center',
+              zIndex: 1,
+            }}
+            size="large"
+            color={theme.background}
+          />
+        ) : null}
+        <FastImage
+          style={styles.poster}
+          source={
+            imagePath
+              ? {
+                  uri: `${imgPrefixOriginal}${
+                    imagePath ? imagePath : fallbackImagePath
+                  }`,
                 }
-                <FastImage
-                    style={styles.poster}
-                    source={imagePath ? {
-                        uri:
-                            `${imgPrefixOriginal}${imagePath ? imagePath : fallbackImagePath}`
-                    } : require('../assets/images/profile_default.png')}
-                    resizeMode={resizeMode}
-                    tintColor={tintColor}
-                    onLoad={() => setIsImageLoading(false)}
-                />
-            </View>
-            <LinearGradient
-                style={styles.headerGradient}
-                colors={[
-                    'rgba(0, 0, 0, 0)',
-                    'rgba(0, 0, 0, 0)',
-                    'rgba(0, 0, 0, 0)',
-                    theme.accent,
-                ]}
-            />
-
-            {/* title */}
-            <View style={styles.titleContainer}>
-                <View>
-
-                </View>
-                <Text style={styles.headerTitle}>
-                    {title}
-                </Text>
-                {subtitle ?
-                    <Text style={styles.subtitle}>
-                        {subtitle}
-                    </Text> : null
-                }
-
-            </View>
-        </View>
-    )
+              : require('../assets/images/profile_default.png')
+          }
+          resizeMode={resizeMode}
+          tintColor={tintColor}
+          onLoad={() => setIsImageLoading(false)}
+        />
+      </View>
+      <LinearGradient
+        style={styles.headerGradient}
+        colors={[
+          'rgba(0, 0, 0, 0)',
+          'rgba(0, 0, 0, 0)',
+          'rgba(0, 0, 0, 0)',
+          theme.accent,
+        ]}
+      />
+    </View>
+  );
 }
