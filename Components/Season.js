c151a72a8c6e93a764a82a029f0a630ec402a99a
halaxysounds <halaxysounds@gmail.com>
2022-04-02 21:04:59 -0300

    refactor

--- Components/Season.js
+++ Components/Season.js
@@ -1,173 +1,180 @@
-import React from 'react'
-import { useContext, useState } from 'react'
+import React from 'react';
+import {useContext, useState} from 'react';
 import {
-    View,
-    Image,
-    Text,
-    StyleSheet,
-    Pressable,
-    Linking,
-    LayoutAnimation
-} from 'react-native'
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  Pressable,
+  Linking,
+  LayoutAnimation,
+} from 'react-native';
 
-import { ThemeContext } from './Contexts/ThemeContext'
-import { imgPrefixLow } from './Utilities/Utilities'
+import {ThemeContext} from './Contexts/ThemeContext';
+import {imgPrefixLow} from './Utilities/Utilities';
 
-import { useNavigation } from '@react-navigation/native'
+import {useNavigation} from '@react-navigation/native';
 
+import {default as MaterialCommunityIcons} from 'react-native-vector-icons/MaterialCommunityIcons';
 
-import { default as MaterialCommunityIcons } from 'react-native-vector-icons/MaterialCommunityIcons'
+export default function Season({data, showId}) {
+  const theme = useContext(ThemeContext);
+  const navigation = useNavigation();
 
-export default function Season({ data, showId, }) {
-    const theme = useContext(ThemeContext)
-    const navigation = useNavigation()
+  const [showAll, setShowAll] = useState(false);
 
-    const [showAll, setShowAll] = useState(false)
+  const styles = StyleSheet.create({
+    section: {
+      paddingHorizontal: theme.defaultPadding,
+      width: '100%',
+    },
+    sectionTitle: {
+      fontSize: 16,
+      fontFamily: theme.fontBold,
+      color: theme.foreground,
+      marginBottom: 10,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    posterImage: {
+      height: 50 * 1.4,
+      width: 50,
+      borderRadius: theme.borderRadius / 2,
+      backgroundColor: theme.accent,
+    },
+    listItem: {
+      flexDirection: 'row',
+      backgroundColor: theme.gray,
+      borderRadius: theme.borderRadius,
+      padding: 10,
+      marginHorizontal: theme.defaultPadding,
+      marginTop: 10,
+    },
+    text: {
+      marginLeft: 10,
+      justifyContent: 'center',
+    },
+    name: {
+      color: theme.foreground,
+      fontFamily: theme.fontBold,
+    },
+    episodeCount: {
+      color: theme.foreground,
+      fontFamily: theme.fontRegular,
+    },
+    airDate: {
+      color: theme.foreground,
+      fontFamily: theme.fontRegular,
+      opacity: 0.3,
+    },
+    showMore: {
+      justifyContent: 'center',
+      marginTop: 15,
+      width: 130,
+      alignSelf: 'center',
+      borderRadius: theme.borderRadius * 2,
+      backgroundColor: theme.gray,
+      padding: 10,
+    },
+  });
 
-    const styles = StyleSheet.create({
-        section: {
-            paddingHorizontal: theme.defaultPadding,
-            width: '100%',
-        },
-        sectionTitle: {
-            fontSize: 16,
-            fontFamily: theme.fontBold,
-            color: theme.foreground,
-            marginBottom: 10,
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        posterImage: {
-            height: 50 * 1.4,
-            width: 50,
-            borderRadius: theme.borderRadius / 2,
-            backgroundColor: theme.accent,
-        },
-        listItem: {
-            flexDirection: 'row',
-            backgroundColor: theme.gray,
-            borderRadius: theme.borderRadius,
-            padding: 10,
-            marginHorizontal: theme.defaultPadding,
-            marginTop: 10
-        },
-        text: {
-            marginLeft: 10,
-            justifyContent: 'center',
-        },
-        name: {
-            color: theme.foreground,
-            fontFamily: theme.fontBold
-        },
-        episodeCount: {
-            color: theme.foreground,
-            fontFamily: theme.fontRegular,
-        },
-        airDate: {
-            color: theme.foreground,
-            fontFamily: theme.fontRegular,
-            opacity: 0.3
-        },
-        showMore: {
-            justifyContent: 'center',
-            marginTop: 15,
-            width: 130,
-            alignSelf: 'center',
-            borderRadius: theme.borderRadius * 2,
-            backgroundColor: theme.gray,
-            padding: 10
-        }
-    })
+  function openLink() {
+    Linking.openURL(tmdbLink ? tmdbLink : 'https://tmdb.com');
+  }
 
-    function openLink() {
-        Linking.openURL(tmdbLink ? tmdbLink : 'https://tmdb.com')
-    }
+  return (
+    <View>
+      <Text
+        style={{
+          ...styles.sectionTitle,
+          ...styles.section,
+          marginTop: 30,
+        }}>
+        Seasons ({data.length})
+      </Text>
 
-    return (
-        <View>
+      {data.slice(0, showAll ? data.length : 3).map((item, index) => {
+        return (
+          <Pressable
+            onPress={() =>
+              navigation.push('season', {
+                showId,
+                seasonNumber: item.season_number,
+              })
+            }
+            style={{marginRight: 7}}
+            key={index}>
+            <View style={styles.listItem}>
+              <Image
+                style={styles.posterImage}
+                source={
+                  item.poster_path
+                    ? {uri: `${imgPrefixLow}${item.poster_path}`}
+                    : require('../assets/images/profile_default.png')
+                }
+              />
 
-            <Text
-                style={{
-                    ...styles.sectionTitle,
-                    ...styles.section,
-                    marginTop: 30,
-                }}
-            >
-                Seasons ({data.length})
-            </Text>
+              <View style={styles.text}>
+                <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                  <Text
+                    style={{
+                      ...styles.name,
+                    }}>
+                    {item.name ? item.name : 'Unknown name'}
+                  </Text>
+                  <MaterialCommunityIcons
+                    name="chevron-right"
+                    color={theme.foreground}
+                    size={16}
+                    style={{marginRight: 15}}
+                  />
+                </View>
+                <View>
+                  <Text style={styles.episodeCount}>
+                    {item.episode_count ? item.episode_count + ' Episodes' : ''}
+                  </Text>
+                  <Text style={styles.airDate}>
+                    {item.air_date ? item.air_date.replaceAll('-', '/') : ''}
+                  </Text>
+                </View>
+              </View>
 
-            {data.slice(0, showAll ? data.length : 3).map((item, index) => {
-                return (
-                    <Pressable
-                        onPress={() => navigation.push('season', { showId, seasonNumber: item.season_number })}
-                        style={{ marginRight: 7 }}
-                        key={index}
-                    >
-                        <View
-                            style={styles.listItem}
-                        >
-                            <Image
-                                style={styles.posterImage}
-                                source={
-                                    item.poster_path ?
-                                        { uri: `${imgPrefixLow}${item.poster_path}` } :
-                                        require('../assets/images/profile_default.png')}
-                            />
-
-                            <View style={styles.text}>
-                                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                                    <Text style={{
-                                        ...styles.name,
-                                    }}>
-                                        {item.name ? item.name : 'Unknown name'}
-                                    </Text>
-                                    <MaterialCommunityIcons
-                                        name="chevron-right"
-                                        color={theme.foreground}
-                                        size={16}
-                                        style={{ marginRight: 15 }}
-                                    />
-                                </View>
-                                <View>
-                                    <Text style={styles.episodeCount}>
-                                        {item.episode_count ? item.episode_count + ' Episodes' : ''}
-                                    </Text>
-                                    <Text style={styles.airDate}>
-                                        {item.air_date ? item.air_date.replaceAll('-', '/') : ''}
-                                    </Text>
-                                </View>
-                            </View>
-
-                            {item.character && showCharacter ?
-                                <Text
-                                    style={{
-                                        ...styles.name,
-                                        color: theme.accentLight,
-                                    }}>
-                                    {item.character ? item.character : 'Unknown Character'}
-                                </Text> : null
-                            }
-                        </View>
-                    </Pressable>
-                )
-            })}
-            {data.length > 3 ? <Pressable
-                style={styles.showMore}
-                onPress={() => {
-                    LayoutAnimation.configureNext({
-                        duration: 500,
-                        update: {
-                            type: 'spring',
-                            springDamping: 0.7,
-                        },
-                    })
-                    setShowAll(!showAll)
-                }}
-            >
-                <Text style={{ ...styles.name, textAlign: 'center', color: theme.foreground }}>
-                    {showAll ? 'Show less...' : 'Show all...'}
+              {item.character && showCharacter ? (
+                <Text
+                  style={{
+                    ...styles.name,
+                    color: theme.accentLight,
+                  }}>
+                  {item.character ? item.character : 'Unknown Character'}
                 </Text>
-            </Pressable> : null}
-        </View >
-    )
+              ) : null}
+            </View>
+          </Pressable>
+        );
+      })}
+      {data.length > 3 ? (
+        <Pressable
+          style={styles.showMore}
+          onPress={() => {
+            LayoutAnimation.configureNext({
+              duration: 500,
+              update: {
+                type: 'spring',
+                springDamping: 0.7,
+              },
+            });
+            setShowAll(!showAll);
+          }}>
+          <Text
+            style={{
+              ...styles.name,
+              textAlign: 'center',
+              color: theme.foreground,
+            }}>
+            {showAll ? 'Show less...' : 'Show all...'}
+          </Text>
+        </Pressable>
+      ) : null}
+    </View>
+  );
 }
